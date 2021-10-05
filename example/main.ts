f4cd98164bc12b5b040aea1c98c2f2c2479e17c4
Erik Onarheim <erik.onarheim@gmail.com>
2021-10-04 20:21:23 -0500

    add get sprite example

--- example/main.ts
+++ example/main.ts
@@ -1,4 +1,4 @@
-import { Actor, DisplayMode, Engine, Loader, vec } from "excalibur";
+import { Actor, DisplayMode, Engine, Loader, Sprite, vec } from "excalibur";
 import { AsepriteResource } from "@excalibur-aseprite";
 
 const game = new Engine({
@@ -13,5 +13,12 @@ const loader = new Loader([asepriteSpriteSheet]);
 game.start(loader).then(()=>{
     const a = new Actor({pos: vec(100, 100)});
     a.graphics.use(asepriteSpriteSheet.getAnimation('Loop') as any);
+
+    const spritesheet = asepriteSpriteSheet.getSpriteSheet();
+    const sprite = spritesheet?.getSprite(0, 1) as Sprite;
+
+    const b = new Actor({pos: vec(200, 200)});
+    b.graphics.use(sprite);
     game.currentScene.add(a);
+    game.currentScene.add(b);
 });
