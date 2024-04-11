55f975036f8870dc71f8d156c3f509f0c8247c52
Erik Onarheim <erik.onarheim@gmail.com>
2024-04-10 22:20:20 -0500

    switch to playwright

--- test/integration/test.js
+++
@@ -1,6 +0,0 @@
-const { test, expectLoaded, expectPage } = require('@excaliburjs/testing')
-
-test('A Aseprite parsed resource', async (page) => {
-    await expectLoaded();
-    await expectPage('Aseprite Loop Animation', './test/integration/images/actual.png').toBe('./test/integration/images/expected.png');
-});
